2124610461ded1d036effdcb90e4a1b51532ff7a
DevWintery <dev.wintery@gmail.com>
2025-02-27 18:12:09 +0900

    ADD: 2065A & 2065B

---
+++ 2065B.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    //operation
+    bool ok = false;
+    for(int i = 0; i < s.length() - 1; i ++)
+    {
+        if(s[i] == s[i + 1])
+            ok = true;
+    }
+    
+    cout << (ok ? 1 : s.length()) << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
