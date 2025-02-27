dcbaa5f3ed8774ea4d215441fb6ca6c971b92b87
DevWintery <dev.wintery@gmail.com>
2025-02-27 18:18:18 +0900

    ADD: 2065A & 2065B

---
+++ 2065A.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+ 
+using namespace std;
+ 
+void solve()
+{
+    string w;
+    cin >> w;
+ 
+    w.replace(w.length() - 2, 2, "i");
+    
+    cout << w << endl;
+}
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
